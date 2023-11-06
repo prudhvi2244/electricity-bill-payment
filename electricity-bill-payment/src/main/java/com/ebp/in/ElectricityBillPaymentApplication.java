ebef07806d730ef1f3012bc5007b65c005e813a8
P <prudhviraj.p@capgemini.com>
2023-11-06 17:08:28 +0530

    First Commit

---
+++ electricity-bill-payment/src/main/java/com/ebp/in/ElectricityBillPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.ebp.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityBillPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityBillPaymentApplication.class, args);
+	}
+
+}
